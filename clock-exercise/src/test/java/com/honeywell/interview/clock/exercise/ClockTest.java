91e8aeecfe15211b4757a7b5d93804b251bfc683
anallathambi <anallathambi@digital.ai>
2020-12-12 12:11:24 +0530

    test case

--- clock-exercise/src/test/java/com/honeywell/interview/clock/exercise/ClockTest.java
+++ clock-exercise/src/test/java/com/honeywell/interview/clock/exercise/ClockTest.java
@@ -1,24 +1,15 @@
 package com.honeywell.interview.clock.exercise;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.honeywell.interview.clock.exercise.model.Time;
-import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,9 +25,6 @@ public class ClockTest {
 
     @Test
     public void shouldGetAngle() throws Exception {
-        Time timeOb = new Time(03, 00, 0);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestJson = objectMapper.writeValueAsString(timeOb);
 
 
         mockMvc.perform(get("/angle/" + "03:00"))
